55440c2f3162fa3deff7241f2068687fccc2974c
Monica Hinga <monicahinga@pop-os.localdomain>
2022-07-26 13:43:22 -0600

    Finalize demo.

--- demo.py
+++ demo.py
@@ -3,11 +3,9 @@ import reader, helper_functions
 class Demo:
     def __init__(self, demo_type:str) -> None:
         self.demo_type = demo_type
-        src_file = 'resources/three_dimensional/three_dimensional_images/2.nii'
-        ground_truth_src_file = 'resources/three_dimensional/three_dimensional_masks/2.nii'
-        bronchial_tree_segmentation_src_file = 'bronchial_segmentations/segmented_volumes/2.p'
-        # src_file = 'resources/0708/00/volume-covid19-A-0708_01.29.nii'
-        # bronchial_tree_segmentation_src_file = 'bronchial_segmentations/708/00.p'
+        src_file = 'resources/three_dimensional/three_dimensional_images/1.nii'
+        ground_truth_src_file = 'resources/three_dimensional/three_dimensional_masks/1.nii'
+        bronchial_tree_segmentation_src_file = 'bronchial_segmentations/segmented_volumes/1.p'
         if demo_type == 's':
             print("Begin 3D segmentation demo.")
             read = reader.Reader(src_file, 0, 4, ground_truth_src_file, False, True, bronchial_tree_segmentation_src_file)
